2ee5663e8bf6fbead29d0edf62a770f68ca756e5
Samvel Petrosov <sam.petrosov@gmail.com>
2021-02-18 11:46:34 -0800

    Initial commit

---
+++ Acumatica-ChangeID/AssetMaintExt.cs
@@ -0,0 +1,17 @@
+﻿using PX.Data;
+using PX.Objects.FA;
+
+namespace Acumatica_ChangeID
+{
+    // Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+    public class AssetMaintExt : PXGraphExtension<AssetMaint>
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            this.Base.action.AddMenuAction(ChangeID);
+        }
+
+        public PXChangeID<FixedAsset, FixedAsset.assetCD> ChangeID;
+    }
+}
